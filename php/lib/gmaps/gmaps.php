dbbddcb1e3cb6bf57c0c8006d05d71bf1efa5032
Reid Welliver <Reid Welliver>
2015-01-17 13:41:18 -0500

    centralizing php gmaps lib functions

--- php/lib/gmaps/gmaps.php
+++ php/lib/gmaps/gmaps.php
@@ -1,6 +1,27 @@
 <?
 require_once('../basicHTTPRequest.php');
 
-$API_KEY = "AIzaSyDRX-KmL574LOnX3fr58DPkOTwjvxvgJVQ";
+class gmaps{
+	private $API_KEY = "AIzaSyDRX-KmL574LOnX3fr58DPkOTwjvxvgJVQ";
+
+	//gets a route from a location
+
+	private buildResponse($data){
+		$response = [
+	        	'error' => "",
+	        	'success' => false,
+		        'data' => data == false ? (is_array($data) ? $data : [$data] )  : [] ; 
+		];
+	}
+
+	public getRouteFromLocations($from,$to){
+		$response = json_decode(request("https://maps.googleapis.com/maps/api/directions/json?origin=$from&destination=$to&key=$API_KEY"););
+		return $response;
+	}
+
+}
+
+
+
 
 ?>
