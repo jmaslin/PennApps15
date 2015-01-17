4e6a1e707c9d52b7e243a7bf044bb5abe8d2556c
Adam Jablonski <adamsjablonski@gmail.com>
2015-01-17 03:22:44 -0500

    work on route and point

--- php/route/getFromLocations.php
+++ php/route/getFromLocations.php
@@ -1,1 +1,27 @@
+<?
+include('../lib/basicHTTPRequest.php');
 //gets a route from a location
+
+$fromLoc = isset($_REQUEST['from']) ? $_REQUEST['from'] : false;
+$toLoc = isset($_REQUEST['to']) ? $_REQUEST['to'] : false;
+
+$response = [
+	'error' => "",
+	'success' => false,
+	'data' => []
+];
+
+//write a function that echoes a json-encoded error array
+echo request("google.com");
+
+//write a function to make a GET request to google taking params (to,from)
+
+
+
+//return a structure 
+
+
+
+
+
+?>
